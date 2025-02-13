629662fb1498fce34adffba8cf3abed0c1075382
picocico <sub.pipi.vivi@gmail.com>
2025-02-13 21:30:19 +0900

    コードの整形

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -14,51 +14,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	private String name = "Pico Cico";
-	private String age = "31";
+  private String name = "Pico Cico";
+  private String age = "31";
 
-	// Mapを作成
-	private Map<String, String> students = new HashMap<>();
+  // Mapを作成
+  private Map<String, String> students = new HashMap<>();
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
-	// GETは取得する、リクエストの結果を受け取る
-	@GetMapping("/studentInfo")
-	public String getStudentInfo() {
-		return name + " " + age + "歳";
-	}
+  // GETは取得する、リクエストの結果を受け取る
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
+  }
 
-	// MapをGETする
-	@GetMapping("/studentMap")
-	public Map <String, String> getStudentMap() {
-		return students;
-	}
+  // MapをGETする
+  @GetMapping("/studentMap")
+  public Map<String, String> getStudentMap() {
+    return students;
+  }
 
-	// POSTは情報を与える、渡す
-	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name, String age) {
-		this.name = name;
-	  this.age = age;
-	}
+  // POSTは情報を与える、渡す
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
   // nameだけPOSTする
-	@PostMapping("/studentName")
-	public void updateStudentName(String name) {
-		this.name = name;
-	}
+  @PostMapping("/studentName")
+  public void updateStudentName(String name) {
+    this.name = name;
+  }
 
-	// MapにPOSTする
-	@PostMapping("/studentMap")
-	public void addStudentMap(String studentNumber, String studentName) {
-		students.put(studentNumber,studentName);
-	}
+  // MapにPOSTする
+  @PostMapping("/studentMap")
+  public void addStudentMap(String studentNumber, String studentName) {
+    students.put(studentNumber, studentName);
+  }
 
-	// MapからDeleteする
-	@DeleteMapping("/studentMap")
-	public void deleteStudentMap(@RequestParam String studentNumber) {
-		students.remove(studentNumber);
-
-	}
+  // MapからDeleteする
+  @DeleteMapping("/studentMap")
+  public void deleteStudentMap(@RequestParam String studentNumber) {
+    students.remove(studentNumber);
+  }
 }
 
