fc837047688c2b89798d781bc142ef92d496d938
picocico <sub.pipi.vivi@gmail.com>
2025-02-19 21:00:40 +0900

    学生情報のリスト取得とCRUD機能の実装

---
+++ src/main/java/raisetech/student/management/Student.java
@@ -0,0 +1,32 @@
+package raisetech.student.management;
+
+public class Student {
+
+  private int id; //IDを追加
+  private String name;
+  private int age;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+}
