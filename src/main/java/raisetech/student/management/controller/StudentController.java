9a78db988434e73f6449588c937d6ca652d3b955
picocico <sub.pipi.vivi@gmail.com>
2025-04-19 19:45:25 +0900

    ・ワイルドカードインポートを回避し、個別インポートへ修正

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -1,19 +1,29 @@
 package raisetech.student.management.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
-import raisetech.student.management.dto.*;
+import raisetech.student.management.dto.StudentDetailDto;
+import raisetech.student.management.dto.StudentRegistrationRequest;
 import raisetech.student.management.service.StudentService;
 
-import java.util.List;
 
 /**
  * 学生に関するREST APIを提供するコントローラークラス。
